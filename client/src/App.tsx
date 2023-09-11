c0727d20feed2ab4e19c3fedb0a08d9ffb0b80c6
Bryce Nguyen <bknguyen@ualberta.ca>
2023-09-10 22:28:59 -0600

    feature: themes for dark and light mode and top bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {ColorModeContext, useMode} from "./theme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
 import AuthenticationForm from "./pages/auth/AuthenticationForm";
-import { Account } from "./context/Account";
+import {Account} from "./context/Account";
 import Status from "./pages/Status";
 import Settings from "./pages/Settings";
+import Topbar from "./pages/global/Topbar";
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-            <Account>
-                <Status />
-                <Routes>
-                    <Route path="/signup" element={<AuthenticationForm mode="signup" />}/>
-                    <Route path="/login" element={<AuthenticationForm mode="login" />}/>
-                </Routes>
-                <Settings />
-            </Account>
-        </BrowserRouter>
-    </div>
-  );
+    const [theme, colorMode] = useMode();
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="App">
+                    <Topbar />
+                    <BrowserRouter>
+                        <Account>
+                            <Status/>
+                            <Routes>
+                                <Route path="/signup" element={<AuthenticationForm mode="signup"/>}/>
+                                <Route path="/login" element={<AuthenticationForm mode="login"/>}/>
+                            </Routes>
+                            <Settings/>
+                        </Account>
+                    </BrowserRouter>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
