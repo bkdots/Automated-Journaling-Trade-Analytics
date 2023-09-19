1475caec76919047cb30fbdc1fc69b0253ecc0bd
Bryce Nguyen <bknguyen@ualberta.ca>
2023-09-18 18:47:54 -0600

    feature: tabs component and example of usage

---
+++ client/src/components/TabsComponent.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+interface TabData {
+    label: string;
+    content: React.ReactNode;
+    route: string;
+}
+
+interface TabsComponentProps {
+    tabs: TabData[];
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const TabsComponent: React.FC<TabsComponentProps> = ({ tabs }) => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const currentTabRoute = new URLSearchParams(location.search).get("tab");
+    const value = tabs.findIndex(tab => tab.route === currentTabRoute) || 0;
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        navigate(`?tab=${tabs[newValue].route}`);
+    };
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    {tabs.map((tab, index) => (
+                        <Tab key={index} label={tab.label} {...a11yProps(index)} />
+                    ))}
+                </Tabs>
+            </Box>
+            {tabs.map((tab, index) => (
+                <CustomTabPanel key={index} value={value} index={index}>
+                    {tab.content}
+                </CustomTabPanel>
+            ))}
+        </Box>
+    );
+}
+
+export default TabsComponent;
--- client/src/pages/journal/index.tsx
+++ client/src/pages/journal/index.tsx
@@ -1,15 +1,147 @@
-import { Routes, Route } from 'react-router-dom';
-import Header from "../../components/Header"
+import { useLocation } from 'react-router-dom';
 import { Box } from "@mui/material";
+import TabsComponent from '../../components/TabsComponent';
 
 const Journal = () => {
+    const location = useLocation();
+
+    const currentTabIndex = location.pathname.includes('tags') ? 1 : 0;
 
     return (
         <Box m="20px">
-            <Routes>
-            </Routes>
+            <TabsComponent
+                tabs={[
+                    { label: "Item One", content: <div>Content for Item One</div>, route: "item-one" },
+                    { label: "Item Two", content: <div>Content for Item Two</div>, route: "item-two"},
+                ]}
+            />
         </Box>
     );
 };
 
 export default Journal;
+
+// import React, { useEffect, useState } from 'react';
+// import { useNavigate, Link } from 'react-router-dom';
+// import {
+//     Grid, Paper, Table, TableBody, TableCell, TableHead, TableRow,
+//     Button, TextField, IconButton, Tooltip, Links
+// } from '@mui/material';
+// import { Delete as DeleteIcon, Edit as EditIcon, Done as DoneIcon, Clear as ClearIcon } from '@mui/icons-material';
+// import axios from 'axios';
+// import { useStateContext } from '../contexts/ContextProvider';
+//
+// const Journals = () => {
+//     const { user, journals, setJournals } = useStateContext();
+//     const navigate = useNavigate();
+//     const [journalName, setJournalName] = useState("");
+//     const [journalDescription, setJournalDescription] = useState("");
+//     const [editingJournalId, setEditingJournalId] = useState(null);
+//
+//     const apiConfig = (method = "GET", data = {}) => ({
+//         method,
+//         headers: {
+//             Authorization: `Bearer ${user.token}`,
+//         },
+//         data
+//     });
+//
+//     const fetchJournals = async () => {
+//         try {
+//             const { data } = await axios(apiConfig());
+//             setJournals(data);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     };
+//
+//     useEffect(() => {
+//         fetchJournals();
+//     }, []);
+//
+//     return (
+//         <div>
+//             <Grid container spacing={3}>
+//                 <Grid item xs={12}>
+//                     <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+//                         <h2>Journals</h2>
+//                         <Button variant="contained" onClick={() => navigate('/dashboard/journal/add')}>+ Create a Journal</Button>
+//                         <JournalTable {...{ journals, editingJournalId, setEditingJournalId, setJournalName, setJournalDescription, journalName, journalDescription }} />
+//                         <Links color="primary" href="#" onClick={(e) => e.preventDefault()} sx={{ mt: 3 }}>
+//                             See more orders
+//                         </Links>
+//                     </Paper>
+//                 </Grid>
+//             </Grid>
+//         </div>
+//     );
+// };
+//
+// const JournalTable = ({ journals, editingJournalId, setEditingJournalId, setJournalName, setJournalDescription, journalName, journalDescription }) => (
+//     <Table size="small">
+//         <TableHead>
+//             <TableRow>
+//                 <TableCell>Name</TableCell>
+//                 <TableCell>Description</TableCell>
+//                 <TableCell>Actions</TableCell>
+//             </TableRow>
+//         </TableHead>
+//         <TableBody>
+//             {journals.map((journal) => (
+//                 <JournalRow key={journal._id} {...{ journal, editingJournalId, setEditingJournalId, setJournalName, setJournalDescription, journalName, journalDescription }} />
+//             ))}
+//         </TableBody>
+//     </Table>
+// );
+//
+// const JournalRow = ({ journal, editingJournalId, setEditingJournalId, setJournalName, setJournalDescription, journalName, journalDescription }) => (
+//     <TableRow key={journal._id}>
+//         <TableCell>
+//             {editingJournalId === journal._id ? (
+//                 <TextField value={journalName} onChange={(e) => setJournalName(e.target.value)} />
+//             ) : (
+//                 journal.journalName
+//             )}
+//         </TableCell>
+//         <TableCell>
+//             {editingJournalId === journal._id ? (
+//                 <TextField value={journalDescription} onChange={(e) => setJournalDescription(e.target.value)} />
+//             ) : (
+//                 journal.journalDescription
+//             )}
+//         </TableCell>
+//         <TableCell>
+//             {editingJournalId === journal._id ? (
+//                 <>
+//                     <Tooltip title="Done">
+//                         <IconButton onClick={() => handleEditSubmit(journal._id)}>
+//                             <DoneIcon />
+//                         </IconButton>
+//                     </Tooltip>
+//                     <Tooltip title="Clear">
+//                         <IconButton onClick={() => setEditingJournalId(null)}>
+//                             <ClearIcon />
+//                         </IconButton>
+//                     </Tooltip>
+//                 </>
+//             ) : (
+//                 <>
+//                     <Tooltip title="Edit">
+//                         <IconButton onClick={() => setEditingJournalId(journal._id)}>
+//                             <EditIcon />
+//                         </IconButton>
+//                     </Tooltip>
+//                     <Tooltip title="Delete">
+//                         <IconButton onClick={() => handleDeleteSubmit(journal._id)}>
+//                             <DeleteIcon />
+//                         </IconButton>
+//                     </Tooltip>
+//                 </>
+//             )}
+//         </TableCell>
+//     </TableRow>
+// );
+//
+// // ... other code remains the same ...
+//
+// export default Journals;
