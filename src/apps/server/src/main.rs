fcf727afd0cca5bd22d814adede6354f0f7f5a89
ubryce <Bknguyen@ualberta.ca>
2024-05-04 22:48:39 -0600

    feature: change hello route to a handler and log into server with route

--- src/apps/server/src/main.rs
+++ src/apps/server/src/main.rs
@@ -1,21 +1,34 @@
 #![allow(unused)]
 
-use axum::Router;
-use axum::routing::get;
-use axum::response::Html;
+use axum::extract::{Path, Query};
+use axum::http::{Method, Uri};
+use axum::response::{Html, IntoResponse, Response};
+use axum::routing::{get, get_service};
+use axum::{middleware, Json, Router};
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let routes_hello = Router::new().route(
         "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+        get(handler_hello)
     );
 
+    // region:  --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
         .serve(routes_hello.into_make_service())
         .await
         .unwrap();
+    // endregion:   ---Start Server
 }
+
+// region:  --- Handler Hello
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
+    Html("Hello <strong>World!!!</strong>")
+}
+// endregion:   --- Handler Hello
