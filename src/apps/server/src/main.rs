937383e479b69c6b818743173c4da374790445c6
ubryce <Bknguyen@ualberta.ca>
2024-05-05 12:31:37 -0600

    feature: static routes for fallback service

--- src/apps/server/src/main.rs
+++ src/apps/server/src/main.rs
@@ -9,33 +9,51 @@ use std::net::SocketAddr;
 use tokio::net::TcpListener;
 use serde::Deserialize;
 use serde_json::json;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(handler_hello)
-    );
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     // region:  --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
     // endregion:   ---Start Server
 }
 
-// region:  --- Handler Hello
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+// region:  --- Routes Hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
 #[derive(Debug, Deserialize)]
 struct HelloParams {
     name: Option<String>,
 }
+
+// e.g., `/hello?name=Jen`
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World!");
     Html(format!("Hello <strong>{name}</strong>"))
 }
-// endregion:   --- Handler Hello
+
+// e.g., `/hello2/Mike`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+// endregion:   --- Routes Hello
