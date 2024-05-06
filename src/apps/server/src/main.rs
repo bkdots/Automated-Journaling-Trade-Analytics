eadd6c041d3f49de79801261f336e1ed2ca606dd
ubryce <Bknguyen@ualberta.ca>
2024-05-05 21:54:19 -0600

    feature: require auth token middleware for specific routes

--- src/apps/server/src/main.rs
+++ src/apps/server/src/main.rs
@@ -24,11 +24,15 @@ async fn main() -> Result<()> {
     // Initialize ModelController
     let mc = ModelController::new().await?;
 
+    // Ensure Auth token is needed for these routes
+    let routes_apis = web::routes_tickets::routes(mc.clone())
+        .route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
+
     // The below gets executed form the bottom to the top
     let routes_all = Router::new()
         .merge(routes_hello())
         .merge(web::routes_login::routes())
-        .nest("/api", web::routes_tickets::routes(mc.clone()))
+        .nest("/api", routes_apis)
         .layer(middleware::map_response(main_response_mapper))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
