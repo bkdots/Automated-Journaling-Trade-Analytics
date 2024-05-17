aee209d471fe780e105a0901f76d709c4109cdeb
ubryce <Bknguyen@ualberta.ca>
2024-05-16 18:21:07 -0600

    feature: add proxy to backend server

--- src/apps/client2/next.config.mjs
+++ src/apps/client2/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:8080/api/:path*' // Proxy to Backend
+            }
+        ];
+    }
+};
 
 export default config;
