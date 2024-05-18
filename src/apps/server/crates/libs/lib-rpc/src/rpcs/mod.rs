08cfc5339e11ee7a246fe20c8656b4c53de9faf2
ubryce <Bknguyen@ualberta.ca>
2024-05-18 12:50:55 -0600

    fix: add all rpc endpoints and create list test in quick dev

--- src/apps/server/crates/libs/lib-rpc/src/rpcs/mod.rs
+++ src/apps/server/crates/libs/lib-rpc/src/rpcs/mod.rs
@@ -16,4 +16,11 @@ pub fn all_rpc_router() -> RpcRouter {
 	RpcRouter::new()
 		.extend(agent_rpc::rpc_router())
 		.extend(conv_rpc::rpc_router())
+		.extend(journal_rpc::rpc_router())
+		.extend(apikey_rpc::rpc_router())
+		.extend(exchange_rpc::rpc_router())
+		.extend(tag_rpc::rpc_router())
+		.extend(trade_rpc::rpc_router())
+		.extend(tradetag_rpc::rpc_router())
+		.extend(thread_rpc::rpc_router())
 }
